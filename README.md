0571d08cfc8d2c9e11e20d3a4dd2f43c3a04398b
Hannah Atkinson <hatkinson@lexmark.com>
2013-12-23 11:07:39 -0500

    Initial commit (from hlatki.github.io source branch)

---
+++ README.md
@@ -0,0 +1,10 @@
+This is the source for my personal website/blog. It uses the [Static](http://nakkaya.com/static.html) 
+static site generator.
+
+The generated files for the GitHub pages site are in the hlatki.github.io repo.
+
+To regenerate blog:
+
+```
+java -jar static-app.jar --build
+```
---
+++ config.clj
@@ -0,0 +1,11 @@
+[:site-title "blog.hannah.io"
+ :site-description "Notes on code and such"
+ :site-url "http://blog.hannah.io"
+ :author-name "Hannah Atkinson"
+ :in-dir "resources/"
+ :out-dir "html/"
+ :default-template "default.clj"
+ :encoding "UTF-8"
+ :atmoic-build true
+ :blog-as-index true
+ :create-archives false]
