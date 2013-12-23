f0aff752b160c0d8a4cf85396e4fe9fdc0234668
Hannah Atkinson <hatkinson@lexmark.com>
2013-12-23 14:51:35 -0500

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# deploy the contents of $outout_dir to $repo 
+
+output_dir="html"
+commit_message="Site updated on `date`"
+repo="https://github.com/hlatki/hlatki.github.io.git"
+
+cd $output_dir
+git init 
+git add .
+git commit -m "$commit_message"
+git remote add origin $repo
+#git push origin master --force
