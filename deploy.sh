a02baf22f9d213278b77c8df138439a3fd6f0b8c
hlatki <hannahatkinson@gmail.com>
2014-03-23 14:24:04 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 # deploy the contents of $outout_dir to $repo 
 
-output_dir="html"
+# output_dir="html"
+# output_dir="dist" # if using grunt's output
 commit_message="Site updated on `date`"
 repo="https://github.com/hlatki/hlatki.github.io.git"
 
