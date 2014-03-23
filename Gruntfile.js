44b1c578e98ebe162540a8c03dc4e5041921d8b7
hlatki <hannahatkinson@gmail.com>
2014-03-23 14:02:01 -0700

    Grunt: add cssmin and http-server

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        copy: {
+        copy: { //make copy so we can compare results with Static's output
             dist: {
                 cwd: 'html',
                 expand: true,
@@ -33,12 +33,42 @@ module.exports = function(grunt) {
                     ext: '.html'
                 }]
             }
+        },
+
+       cssmin: {
+            minify: {
+                files: {
+                    'dist/css/compiled.min.css': ['dist/css/compiled.css']
+                }
+            }
+       },
+
+        'http-server': {
+            dev: {
+                    // the server root directory
+                    root: 'dist/',
+
+                    port: 8282,
+                    host: "127.0.0.1",
+
+                    cache: 5,
+                    showDir : true,
+                    autoIndex: true,
+                    defaultExt: "html",
+
+                    //wait or not for the process to finish
+                    runInBackground: false
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['copy', 'uncss','processhtml']);
+    grunt.loadNpmTasks('grunt-http-server');
+    
+
+    grunt.registerTask('default', ['copy', 'uncss','processhtml', 'cssmin']);
 };
